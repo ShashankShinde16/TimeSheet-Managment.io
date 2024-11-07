195886e294e0dfff82eae28621c2cb79928ba646
ShashankShinde16 <ss9575a@gmail.com>
2024-11-07 14:57:15 +0530

    installed react-router-dom axios and login signup setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/login'
+import Signup from './components/Signup'
+import { Route , Routes, BrowserRouter } from 'react-router-dom'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
